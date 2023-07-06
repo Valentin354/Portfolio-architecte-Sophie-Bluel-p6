658ffe37951fe42520dfddeb48e5b47d682f5f44
Valentin Bunel <bunel.valentin@hotmail.fr>
2023-07-06 19:31:45 +0200

    Création login

--- FrontEnd/assets/js/index.js
+++ FrontEnd/assets/js/index.js
@@ -28,28 +28,33 @@ fetch("http://localhost:5678/api/works")
     creaPortfolio(data);
   });
 
-// filtres
+// filtres ---------------------------------------------------------
 
+// Sélection des boutons de filtre
 const filterButtons = document.querySelectorAll(".filter-button");
+// Sélection des éléments de la galerie
 const galleryItems = document.querySelectorAll(".gallery");
 
+// Ajout d'un écouteur événement à chaque bouton de filtre
 filterButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const filterValue = button.getAttribute("data-filter");
-    //si filtervalue=0 alors j'apl cardportfolio ac data all et "return" pour stop
 
+    // Si la valeur est 0, afficher toutes les images
     if (filterValue == 0) {
       creaPortfolio(dataAll);
       return;
     }
-    const filterData = [];
 
+    // Filtrage des données en fonction de la valeur du filtre
+    const filterData = [];
     dataAll.forEach((item) => {
       if (item.categoryId == filterValue) {
         filterData.push(item);
       }
     });
-    console.log(filterData);
+
+    // Affichage de la galerie filtrée
     creaPortfolio(filterData);
   });
 });
